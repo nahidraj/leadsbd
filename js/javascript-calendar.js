99be2981ae02c901fdbd3b99dafe694b2ca3a99f
nahidraj <nahidraj.web@gmail.com>
2024-12-23 19:11:35 +0600

    update

--- js/javascript-calendar.js
+++ js/javascript-calendar.js
@@ -1,23 +1,15 @@
 'use strict';
+
 let dt = new Date();
 
 function renderDate() {
-    let dateString = new Date();
-
-    dt.setDate(1);
-    let day = dt.getDay();
-
+    dt.setDate(1); // Set to the first day of the current month
+    let day = dt.getDay(); // Get the weekday of the first day
     let endDate = new Date(
         dt.getFullYear(),
         dt.getMonth() + 1,
         0
-    ).getDate();
-
-    let prevDate = new Date(
-        dt.getFullYear(),
-        dt.getMonth(),
-        0
-    ).getDate();
+    ).getDate(); // Get the last date of the current month
 
     let today = new Date();
 
@@ -36,43 +28,39 @@ function renderDate() {
         "December"
     ];
 
+    // Set the month and date string
     document.getElementById("icalendarMonth").innerHTML = months[dt.getMonth()] + " , " + dt.getFullYear();
-    document.getElementById("icalendarDateStr").innerHTML = dateString.toDateString();
+    document.getElementById("icalendarDateStr").innerHTML = today.toDateString();
 
     let cells = "";
-    let countDate = 0;
 
-    for (let x = day; x > 0; x--) {
-        cells += "<div class='icalendar__prev-date'>" + (prevDate - x + 1) + "</div>";
+    // Add empty cells for days before the first day of the current month
+    for (let x = 0; x < day; x++) {
+        cells += "<div class='icalendar__empty'></div>";
     }
 
+    // Add cells for the days of the current month
     for (let i = 1; i <= endDate; i++) {
         if (i === today.getDate() && dt.getMonth() === today.getMonth() && dt.getFullYear() === today.getFullYear()) {
             cells += "<div class='icalendar__today'>" + i + "</div>";
         } else {
             cells += "<div>" + i + "</div>";
         }
-
-        countDate = i;
     }
 
-    let reservedDateCells = countDate + day + 1;
-    for (let j1 = reservedDateCells, j2 = 1; j1 <= 42; j1++, j2++) {
-        cells += "<div class='icalendar__next-date'>" + j2 + "</div>";
-    }
-
+    // Set the inner HTML of the calendar days container
     document.getElementsByClassName("icalendar__days")[0].innerHTML = cells;
 }
 
+// Call renderDate initially
 renderDate();
 
-
+// Function to navigate between months
 function moveDate(param) {
     if (param === 'prev') {
         dt.setMonth(dt.getMonth() - 1);
     } else if (param === 'next') {
         dt.setMonth(dt.getMonth() + 1);
     }
-
     renderDate();
 }
