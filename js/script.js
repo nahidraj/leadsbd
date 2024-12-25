eea6c11ae23792fb45e316d1b72dbdb3e4f89360
nahidraj <nahidraj.web@gmail.com>
2024-12-25 17:02:11 +0600

    update

--- js/script.js
+++ js/script.js
@@ -54,12 +54,6 @@
     arrows: false,
     vertical: true,
     dots: false,
-    responsive: [{
-      breakpoint: 992,
-      settings: {
-        slidesToShow: 1,
-      }
-    }]
   });
 
   // date js
@@ -160,8 +154,24 @@
     });
   });
 
-  // lightbox js
- 
+
+  // Mobile menu js start
+  $(".mobile-topbar .bars").on("click", function () {
+    $(".mobile-menu-overlay,.mobile-menu-main").addClass("active");
+  });
+
+  $(".close-mobile-menu,.mobile-menu-overlay").on("click", function () {
+    $(".mobile-menu-overlay,.mobile-menu-main").removeClass("active");
+  });
+
+  $('.sub-mobile-menu ul').hide();
+  $(".sub-mobile-menu a").on("click", function () {
+    $('.sub-mobile-menu ul').not($(this).next("ul")).slideUp(300);
+    $(".sub-mobile-menu a i").not($(this).find("i")).removeClass("fa-chevron-up").addClass("fa-chevron-down");
+    $(this).next("ul").slideToggle(300);
+    $(this).find("i").toggleClass("fa-chevron-up fa-chevron-down");
+  });
+
   
   
   
