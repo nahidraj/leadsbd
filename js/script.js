60cafcf4bca2ca9089a0f338a34c4df98de797ad
nahidraj <nahidraj.web@gmail.com>
2024-12-25 15:17:03 +0600

    update

--- js/script.js
+++ js/script.js
@@ -2,25 +2,29 @@
   "use strict";
 
   // Nivo slider js
-  $(window).load(function () {
+  $(window).on('load', function () {
+    
     $('#slider').nivoSlider({
-      effect: 'random',
-      slices: 15,
-      boxCols: 8,
-      boxRows: 4,
-      animSpeed: 500,
-      pauseTime: 3000,
-      startSlide: 0,
-      directionNav: true,
-      controlNav: false,
-      controlNavThumbs: false,
-      pauseOnHover: true,
-      manualAdvance: false,
-      prevText: '',
-      nextText: '',
-      randomStart: false,
+        effect: 'random',
+        slices: 15,
+        boxCols: 8,
+        boxRows: 4,
+        animSpeed: 500,
+        pauseTime: 3000,
+        startSlide: 0,
+        directionNav: true,
+        controlNav: false,
+        controlNavThumbs: false,
+        pauseOnHover: true,
+        manualAdvance: false,
+        prevText: '',
+        nextText: '',
+        randomStart: false,
+        afterLoad: function() {
+            $('#loader').hide();
+        }
     });
-  });
+});
 
   // Event slider js
   $(".event_slider").slick({
